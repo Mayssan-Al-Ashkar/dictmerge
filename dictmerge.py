f2341f81abcbcf7a4e82017b57d836206719a97c
Mayssan <mayssanalashkar@gmail.com>
2025-03-01 22:32:20 +0200

    testing the code

--- dictmerge.py
+++ dictmerge.py
@@ -3,3 +3,9 @@ def merge_dictionaries(dict1, dict2):
     merged_dict.update(dict2)  
     return merged_dict
 
+
+dict1 = {"a": 1, "b": 2, "c": 3}
+dict2 = {"b": 10, "d": 4}
+
+result = merge_dictionaries(dict1, dict2)
+print(result)
